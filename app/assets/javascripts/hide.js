e8d7158e1486ad2592d27958dfe6db74891ae8ee
Erin Green <erin.y.green@gmail.com>
2019-06-03 19:49:59 +0000

    Automatically backed up by Learn

--- app/assets/javascripts/hide.js
+++ app/assets/javascripts/hide.js
@@ -1,8 +1,8 @@
 
 
-// function hideWhenClicked(event) {
-//   var $target = $(event.target);
-//   $target.hide();
-// }
+function hideWhenClicked(event) {
+  var $target = $(event.target);
+  $target.hide();
+}
 
 $('#hide_this').click(hideWhenClicked);
