9e85904d42001a6bc1a76397920b16bf8415dbbb
Erin Green <erin.y.green@gmail.com>
2019-06-03 19:46:04 +0000

    Automatically backed up by Learn

--- app/assets/javascripts/hide.js
+++ app/assets/javascripts/hide.js
@@ -1,11 +1,18 @@
-let el = document.getElementById("hide_this")
+// let el = document.getElementById("hide_this")
+//
+// function hideWhenClicked(element) {
+//   // document.getElementById("hide_this").onclick = function() {
+//   //   document.getElementById("hide_this").style.display = "none";
+//   // }
+//
+//   element.hide();
+// }
+//
+// $('#hide_this').click(hideWhenClicked)
 
-function hideWhenClicked(element) {
-  // document.getElementById("hide_this").onclick = function() {
-  //   document.getElementById("hide_this").style.display = "none";
-  // }
-
-  element.hide();
+function hideWhenClicked(event) {
+  var $target = $(event.target);
+  $target.hide();
 }
 
-$('#hide_this').click(hideWhenClicked)
+$('#hide_this').click(hideWhenClicked);
